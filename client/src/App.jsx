52dad2c949ad189c912b7d2b1b1a168508c564e8
pamanyuthmika <puthmika@gmail.com>
2024-09-27 13:06:44 +0530

    production changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,42 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import CreateUser from './EmployeeManagement/CreateUser';
 import UpdateUser from './EmployeeManagement/UpdateUser';
 import Users from './EmployeeManagement/Users';
-import Dashboard from './EmployeeManagement/Dashboard'; // Import the Dashboard component
+import ServiceDashboard from './EmployeeManagement/Dashboard'; // Import the Dashboard component
 import Attendance from './EmployeeManagement/Attendance'; // Import Attendance component
 import Payments from './EmployeeManagement/Payments'; // Import Payments component
 import AttendanceRecords from './EmployeeManagement/AttendanceRecords'; // Import the new component
 import Login from './EmployeeManagement/login';
 
-
-
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login />} /> {/* Login page */}
-          <Route path='/dashboard' element={<Dashboard />} /> {/* Dashboard as the landing page */}
-          <Route path='/users' element={<Users />} />
-          <Route path='/create' element={<CreateUser />} />
-          <Route path='/update/:id' element={<UpdateUser />} />
-          <Route path='/attendance' element={<Attendance />} />
-          <Route path='/payments' element={<Payments />} />
-          <Route path='/attendanceRecords' element={<AttendanceRecords />} /> {/* New route */}
-
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;
-
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import Services from './ServiceRecordManagement/Services'
 import CreateService from './ServiceRecordManagement/CreateService'
 import UpdateService from './ServiceRecordManagement/UpdateService'
@@ -65,18 +35,29 @@ function App() {
       <BrowserRouter>
       <PackageHeader/>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/Dashboard' element={<Dashboard />}></Route>
-        <Route path='/create' element={<CreateService />}></Route>
-        <Route path='/update/:id' element={<UpdateService />}></Route>
-        <Route path='/reports' element={<ServiceReports />}></Route>
-        <Route path='/qrCodes' element={<QRCodePage />}></Route>
-        <Route path="/service/:vin" element={<ServiceDetails />} />
-        <Route path="/parts-usage" element={<PartsUsagePieChart />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/SerDescription" element={<SerDescription />} />
-        <Route path="/serviceLogin" element={<ServiceLogin />} />
-        <Route path="/PINPage" element={<PINPage />} />
+        <Route path='services' element={<Home />}>
+          <Route index element={<ServiceDashboard />} />
+          <Route path='create' element={<CreateService />} />
+          <Route path='update/:id' element={<UpdateService />} />
+          <Route path='reports' element={<ServiceReports />} />
+          <Route path='qrCodes' element={<QRCodePage />} />
+          <Route path="service/:vin" element={<ServiceDetails />} />
+          <Route path="parts-usage" element={<PartsUsagePieChart />} />
+          <Route path="services" element={<Services />} />
+          <Route path="SerDescription" element={<SerDescription />} />
+          <Route path="serviceLogin" element={<ServiceLogin />} />
+          <Route path="PINPage" element={<PINPage />} />
+        </Route>
+
+        <Route path='/' element={<Login />} /> {/* Login page */}
+        <Route path='/dashboard' element={<Dashboard />} /> {/* Dashboard as the landing page */}
+        <Route path='/users' element={<Users />} />
+        <Route path='/create' element={<CreateUser />} />
+        <Route path='/update/:id' element={<UpdateUser />} />
+        <Route path='/attendance' element={<Attendance />} />
+        <Route path='/payments' element={<Payments />} />
+        <Route path='/attendanceRecords' element={<AttendanceRecords />} /> {/* New route */}
+        
       </Routes>
       <Footer/>
       </BrowserRouter>
