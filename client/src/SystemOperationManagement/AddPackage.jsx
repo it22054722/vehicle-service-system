e13291fd5b9a88f8526cdd8c7bde402ee769f80f
pasindu maduranga <it22546784@my.sliit.lk>
2024-10-15 23:31:47 +0530

    update pack

--- client/src/SystemOperationManagement/AddPackage.jsx
+++ client/src/SystemOperationManagement/AddPackage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import Swal from 'sweetalert2'; // Import SweetAlert2
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'bootstrap-icons/font/bootstrap-icons.css'; // Import Bootstrap Icons
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Import Bootstrap Icons
 
 export default function AddPackage() {
     const [packageName, setPackageName] = useState("");
